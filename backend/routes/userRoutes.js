abd861acea57a0e228ab758c4138066da8031232
Test <zarzismehdi7@gmail.com>
2025-02-05 11:34:02 +0100

    implementé l'endpoint de réinitialisation de mot de passe

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authenticateJWT = require('../authentification/authenticateToken');
-const { registerUser, loginUser, selectDatabase, getLatestDevisByYear,getAllSectors, sendPasswordResetEmail } = require('../controllers/UserController');
+const { registerUser, loginUser, selectDatabase, getLatestDevisByYear,getAllSectors, sendPasswordResetEmail, passwordReset } = require('../controllers/UserController');
 const { getDevisDetails,getAllClients } = require('../controllers/UserController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/passwordResetRequest', sendPasswordResetEmail);
+router.put('/passwordReset', passwordReset);
 router.post('/select-database', selectDatabase);
 router.get('/get-devis-details/:databaseName/:NUMBL', getDevisDetails);
 
--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -81,7 +81,7 @@ function ResetPassword() {
                                         Email
                                     </label>
                                     <input
-                                        type="email"
+                                        type="text"
                                         id="email"
                                         className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
                                         placeholder="Entrez votre email"
