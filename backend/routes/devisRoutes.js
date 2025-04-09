a62cf4379687ebb6e432f6953c81697e9fdd2795
Test <zarzismehdi7@gmail.com>
2025-04-09 17:05:56 +0100

    a travaillé sur les fichiers: devisRoutes.js, utilisateurRoutes.js, utilisateurController.js, devisController.js
    supprimers des méthodes à partir des routes/controller d'utilisateur

--- backend/routes/devisRoutes.js
+++ backend/routes/devisRoutes.js
@@ -1,21 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getAllClients,
-  getClientByCode,
-  getClientByRsoc,
-  getLibpvByNumbl,
-  getDevisWithDetails,
-
-  getDevisCountByMonthAndYear,
-  getDevisValidees,
-
-  getCodeRepAndRsRep,
-  updateDevis,
-  
-  filterDevis,
-} = require("../controllers/devisControllerPlaceholder");
-const { getAllcodearticle } = require("../controllers/articleController");
 
 const {
   getTousDevis,
@@ -37,7 +21,6 @@ const {
 } = require("../controllers/devisController");
 //devis controller mt3na
 router.post("/:dbName/creerDevis", creerDevis);
-router.get("/:dbName/clients", getAllClients);
 router.get("/:dbName/getNombreDevis", getNombreDevis);
 router.get("/:dbName/getTotalChiffres", getTotalChiffres);
 router.get("/:dbName/getDevisParNUMBL/:NUMBL", getDevisParNUMBL);
@@ -60,25 +43,7 @@ router.get("/:dbName/getListePointVente", getListePointVente);
 router.get("/:dbName/getInfoUtilisateur", getInfoUtilisateur);
 router.get("/:dbName/getListeDevisParCodeClient", getListeDevisParCodeClient)
 //////////////////////////////////////////////
-
-router.get("/:dbName/clients/code/:code", getClientByCode);
-router.get("/:dbName/clients/rsoc/:rsoc", getClientByRsoc);
 //router.get("/:dbName/article", getAllcodearticle);
 router.get("/:dbName/getTousDevis", getTousDevis);
-router.get("/:dbName/devis/libpv/:numbl", getLibpvByNumbl);
-router.get("/:dbName/devis/details", getDevisWithDetails);
-
-router.get(
-  "/devis-count-by-month-and-year/:dbName",
-  getDevisCountByMonthAndYear
-);
-router.get("/:dbName/devis-validees", getDevisValidees);
-router.get(
-  "/get-representant-details/:databaseName/:numbl",
-  getCodeRepAndRsRep
-);
-router.put("/:dbName/:numbl", updateDevis);
-// router.delete("/:dbName/devis/:NUMBL", deleteDevis);
-router.get("/filterDevis", filterDevis);
 
 module.exports = router;
--- backend/routes/utilisateurRoutes.js
+++ backend/routes/utilisateurRoutes.js
@@ -5,35 +5,21 @@ const {
   inscrireUtilisteur,
   loginUtilisateur,
   selectDatabase,
-  getLatestDevisByYear,
-  getAllSectors,
   envoyerDemandeReinitialisationMp,
   reinitialiserMotPasse,
   getUtilisateurParCode
-} = require("../controllers/utilisateurController");
-const {
-  getDevisDetails,
-  getAllClients,
 } = require("../controllers/utilisateurController");
 
 // * inscription pour un utilisateur
 router.post("/inscrireUtilisteur", inscrireUtilisteur);
 // * connexion pour un utilisateur
-router.post("/loginUtilisateur", loginUtilisateur);
+router.post("/  ", loginUtilisateur);
 // * envoyer une demander de réinitialisation de mot de passe
 router.post("/envoyerDemandeReinitialisationMp", envoyerDemandeReinitialisationMp);
 // * réinitialiser un mot de passe
 router.put("/reinitialiserMotPasse", reinitialiserMotPasse);
 // * selectionner la base de données (societé) à gèrer
 router.post("/select-database", selectDatabase);
-// * get a single devis details
-router.get("/get-devis-details/:databaseName/:NUMBL", getDevisDetails);
-// * get latest devis by year
-router.get("/get-devis-details/:databaseName", getLatestDevisByYear);
-// * récuperer la liste des clients d'une société dont l'utilisateur utiliser son erp ?
-router.get("/:databaseName/clients", getAllClients);
-// * récuperer la liste des secteurs ?
-router.get("/secteurs/:databaseName", getAllSectors);
 // * récuperer un utilisateur par son code
 router.get("getUtilisateurParCode/:codeuser", getUtilisateurParCode);
 
