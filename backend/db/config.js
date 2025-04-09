c06a6ea8ddfc09b577285ebc1342e682713b71b7
amenitrigui <amenitriguii2003@gmail.com>
2025-04-09 14:29:08 +0100

    add commentaire

--- backend/db/config.js
+++ backend/db/config.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 // * Informations d'accès au serveur de bases des données
-
+//* test
 const defaultDbConfig = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
