d13fb3ad3ed8b8385ca2030904b46aa0d80d6f93
Test <zarzismehdi7@gmail.com>
2025-04-10 10:47:53 +0100

    supprimer le dossier test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -18,18 +18,12 @@ import GestionDesVentes from "./pages/ErpPages/GestionDesVentes";
 import RegisterPage from "./pages/authentication/RegisterPage";
 import EmailEnvoye from "./pages/authentication/EmailEnvoye";
 import ClientList from "./pages/Clients/ClientList";
-import Parent from "./test/Parent";
 import DevisFormTout from "./pages/Devis/DevisFormTout";
 import DevisFormPlaceholder from "./components/Devis/DevisFormPlaceholder";
 import Deconnexion from "./pages/authentication/Deconnexion";
 import ClientFormTout from "./pages/Clients/ClientFormTout";
-import Test from "./test/Test";
 import { useDispatch, useSelector } from "react-redux";
-import { setActiverBoutonsValiderAnnuler } from "./app/interface_slices/uiSlice";
-import { viderChampsClientInfo } from "./app/client_slices/clientSlice";
-import { setDevisInfo, viderChampsDevisInfo } from "./app/devis_slices/devisSlice";
-import { persistStore } from "redux-persist";
-import { store } from "./app/store";
+import { setDevisInfo } from "./app/devis_slices/devisSlice";
 import UtilisateurFormTout from "./pages/Utilisateurs/UtilisateurFormTout";
 import ArticleFormTout from "./pages/Article/ArticleFormTout"
 import ArticleList from "./pages/Article/ArticleList"
@@ -63,9 +57,7 @@ function App() {
         <Route path="/EmailEnvoye" element={<EmailEnvoye />}></Route>
         <Route path="/ClientList" element={<ClientList />}></Route>
         <Route path="/ClientFormTout" element={<ClientFormTout />} />
-        <Route path="/Parent" element={<Parent />}></Route>
         <Route path="/DevisFormTout" element={<DevisFormTout />}></Route>
-        <Route path="/Test" element={<Test />}></Route>
         <Route path="/ArticleFormTout" element={<ArticleFormTout/>}></Route>
         <Route path="/ArticleList" element={<ArticleList/>}></Route>
 
