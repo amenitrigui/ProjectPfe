35da2d6415077c319a9db51ba6c3f2fcba86dd90
amenitrigui <amenitriguii2003@gmail.com>
2025-04-24 09:55:18 +0100

    modification dans la ligne pour etre responsive

--- frontend/src/components/Devis/ArticlesDevis.jsx
+++ frontend/src/components/Devis/ArticlesDevis.jsx
@@ -73,85 +73,87 @@ function ArticlesDevis() {
   }
   return (
     <div className="space-y-4 p-4 border rounded-md mt-4">
-      <div className="space-y-4 p-4 border rounded-md mt-4">
-        <h3 className="text-lg font-bold">Articles</h3>
-
-        <div className="grid grid-cols-6 gap-4 items-center">
-          <div>
-            <label className="block font-medium">FAMILLE</label>
-            <input
-              type="text"
-              className="border border-gray-300 rounded-md p-2"
-              value={articleInfos.famille}
-              placeholder="Sélectionner ou taper une famille"
-              onChange={(e) => {
-                handlecodeFamilleChange(e.target.value);
-              }}
-
-              onClick={() => {afficherRecherchePopup("famille")}}
-            />
-          </div>
-
-          <div>
-            <label className="block font-medium">CODE ARTICLE</label>
-            <input
-              type="text"
-              className="border border-gray-300 rounded-md p-2"
-              value={articleInfos.code || ""}
-              list="listecodeArticle"
-              placeholder="Sélectionner ou taper un code d'article"
-              onChange={(e) => {
-                handleCodeArticleChange(e.target.value);
-              }}
-              onClick={() => afficherRecherchePopup("article")}
-            />
-          </div>
-          <div>
-            <label className="block font-medium">LIBELLE</label>
-            <input
-              type="text"
-              placeholder="Sélectionner un code article"
-              className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              value={articleInfos.libelle || ""}
-              onChange={(e) => handleChangementChamp("libelle", e)}
-              readOnly
-            />
-          </div>
-
-          <div>
-            <label className="block font-medium">UNITE</label>
-            <input
-              type="text"
-              placeholder="Sélectionner un code article"
-              className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              value={articleInfos.unite || ""}
-              readOnly
-              onChange={(e) => handleChangementChamp("unite", e)}
-            />
-          </div>
-
-          <div>
-            <label className="block font-medium">QUANTITE</label>
-            <input
-              type="text"
-              placeholder="Sélectionner un code article"
-              className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              onChange={(e) => handleChangementChamp("quantite", e)}
-            />
-          </div>
-
-          <div>
-            <label className="block font-medium">CONFIG</label>
-            <textarea
-              placeholder="Sélectionner un code article"
-              className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              value={articleInfos.CONFIG}
-              onChange={(e) => handleChangementChamp("CONFIG", e)}
-            />
-          </div>
+    <div className="space-y-4 p-4 border rounded-md mt-4">
+      <h3 className="text-lg font-bold">Articles</h3>
+  
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 items-center">
+        <div>
+          <label className="block font-medium">FAMILLE</label>
+          <input
+            type="text"
+            className="w-full border border-gray-300 rounded-md p-2"
+            value={articleInfos.famille}
+            placeholder="Sélectionner ou taper une famille"
+            onChange={(e) => {
+              handlecodeFamilleChange(e.target.value);
+            }}
+            onClick={() => {
+              afficherRecherchePopup("famille");
+            }}
+          />
         </div>
-      
-      <div className="grid grid-cols-6 gap-4 items-center">
+  
+        <div>
+          <label className="block font-medium">CODE ARTICLE</label>
+          <input
+            type="text"
+            className="w-full border border-gray-300 rounded-md p-2"
+            value={articleInfos.code || ""}
+            list="listecodeArticle"
+            placeholder="Sélectionner ou taper un code d'article"
+            onChange={(e) => {
+              handleCodeArticleChange(e.target.value);
+            }}
+            onClick={() => afficherRecherchePopup("article")}
+          />
+        </div>
+  
+        <div>
+          <label className="block font-medium">LIBELLE</label>
+          <input
+            type="text"
+            placeholder="Sélectionner un code article"
+            className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            value={articleInfos.libelle || ""}
+            onChange={(e) => handleChangementChamp("libelle", e)}
+            readOnly
+          />
+        </div>
+  
+        <div>
+          <label className="block font-medium">UNITE</label>
+          <input
+            type="text"
+            placeholder="Sélectionner un code article"
+            className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            value={articleInfos.unite || ""}
+            readOnly
+            onChange={(e) => handleChangementChamp("unite", e)}
+          />
+        </div>
+  
+        <div>
+          <label className="block font-medium">QUANTITE</label>
+          <input
+            type="text"
+            placeholder="Sélectionner un code article"
+            className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            onChange={(e) => handleChangementChamp("quantite", e)}
+          />
+        </div>
+  
+        <div>
+          <label className="block font-medium">CONFIG</label>
+          <textarea
+            placeholder="Sélectionner un code article"
+            className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            value={articleInfos.CONFIG}
+            onChange={(e) => handleChangementChamp("CONFIG", e)}
+          />
+        </div>
+      </div>
+  
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 items-center">
         <div>
           <label className="block font-medium">REMISE</label>
           <input
@@ -162,7 +164,7 @@ function ArticlesDevis() {
             onChange={(e) => handleChangementChamp("DREMISE", e)}
           />
         </div>
-
+  
         <div>
           <label className="block font-medium">T.V.A</label>
           <input
@@ -173,7 +175,7 @@ function ArticlesDevis() {
             onChange={(e) => handleChangementChamp("tauxtva", e)}
           />
         </div>
-
+  
         <div>
           <label className="block font-medium">P.U.T.T.C</label>
           <input
@@ -185,6 +187,7 @@ function ArticlesDevis() {
             className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           />
         </div>
+  
         <div>
           <label className="block font-medium">MT NET H.T</label>
           <input
@@ -195,9 +198,9 @@ function ArticlesDevis() {
             readOnly
           />
         </div>
-        <div className="flex-1">
-          <label className="block font-medium">Nbr/Uté </label>
-
+  
+        <div>
+          <label className="block font-medium">Nbr/Uté</label>
           <input
             type="text"
             placeholder="nbrunite"
@@ -206,46 +209,46 @@ function ArticlesDevis() {
             readOnly
           />
         </div>
-        <div className="flex space-x-4">
-          <div className="flex-1">
-            <label className="block font-medium">P.U.H.T</label>
-            <input
-              type="text"
-              step="0.001"
-              placeholder="prix1"
-              value={articleInfos.prix1 || ""}
-              readOnly
-              className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            />
-          </div>
-        </div>
-
-        <div className="flex space-x-2 justify-end">
-          <button
-            className="text-green-500 p-2 border rounded-lg hover:bg-green-100"
-            title="Valider"
-            onClick={handleValiderLDFPBtnClick}
-          >
-            <CheckIcon className="h-6 w-6" />
-          </button>
-
-          <button
-            className="text-blue-500 p-2 border rounded-lg hover:bg-blue-100"
-            title="Modifier"
-          >
-            <PencilIcon className="h-6 w-6" />
-          </button>
-
-          <button
-            className="text-red-500 p-2 border rounded-lg hover:bg-red-100"
-            title="Supprimer"
-          >
-            <TrashIcon className="h-6 w-6" />
-          </button>
+  
+        <div>
+          <label className="block font-medium">P.U.H.T</label>
+          <input
+            type="text"
+            step="0.001"
+            placeholder="prix1"
+            value={articleInfos.prix1 || ""}
+            readOnly
+            className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
         </div>
       </div>
+  
+      <div className="flex flex-col sm:flex-row sm:justify-end space-y-2 sm:space-y-0 sm:space-x-2 mt-4">
+        <button
+          className="text-green-500 p-2 border rounded-lg hover:bg-green-100"
+          title="Valider"
+          onClick={handleValiderLDFPBtnClick}
+        >
+          <CheckIcon className="h-6 w-6" />
+        </button>
+  
+        <button
+          className="text-blue-500 p-2 border rounded-lg hover:bg-blue-100"
+          title="Modifier"
+        >
+          <PencilIcon className="h-6 w-6" />
+        </button>
+  
+        <button
+          className="text-red-500 p-2 border rounded-lg hover:bg-red-100"
+          title="Supprimer"
+        >
+          <TrashIcon className="h-6 w-6" />
+        </button>
       </div>
     </div>
+  </div>
+  
   );
 }
 
