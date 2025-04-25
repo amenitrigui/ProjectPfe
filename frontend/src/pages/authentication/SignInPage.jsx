fd233dc1228e2246c5caac2877c9c7f2e6097c31
Test <zarzismehdi7@gmail.com>
2025-04-25 16:31:58 +0100

    modifier la méthode de connexion avec le bd utilisateurs pour utiliser le paramètre provenant de fichier .env

--- frontend/src/pages/authentication/SignInPage.jsx
+++ frontend/src/pages/authentication/SignInPage.jsx
@@ -61,7 +61,7 @@ function SignInPage() {
   };
 
   useEffect(() => {
-    toast("Date mise à jour 25/04/2025 15:43", {
+    toast("Date mise à jour 25/04/2025 16:32", {
       position: "top-right",
       autoClose: 5000,
       hideProgressBar: false,
