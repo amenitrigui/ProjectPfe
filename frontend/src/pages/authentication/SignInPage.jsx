11ed9cae0c3812bec0b8df807a5f05d128979155
Test <zarzismehdi7@gmail.com>
2025-04-28 16:05:28 +0100

    maj date de maj

--- frontend/src/pages/authentication/SignInPage.jsx
+++ frontend/src/pages/authentication/SignInPage.jsx
@@ -38,7 +38,7 @@ function SignInPage() {
   };
 
   useEffect(() => {
-    toast("Date mise à jour 28/04/2025 8:52", {
+    toast("Date mise à jour 28/04/2025 16:65", {
       position: "top-right",
       autoClose: 5000,
       hideProgressBar: false,
