c6d182136d33e224f9bbf6b917ee3dfb03bd9199
Test <zarzismehdi7@gmail.com>
2025-06-04 15:48:50 +0100

    changement de configuration de cors pour permettre à toutes origines d'interagir avec l'api

--- backend/server.js
+++ backend/server.js
@@ -26,12 +26,7 @@ const app = express();
 
 // Configuration CORS
 const corsOptions = {
-  origin: [
-    process.env.FRONTEND_URL_LOCAL, 
-    process.env.FRONTEND_URL_DISTANT,
-    process.env.FRONTEND_URL_DISTANT+'/',
-    "http://localhost:3000"
-  ],
+  origin: ['*'],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   // credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
